a568cb9741ce7ab60a517b94689fbed397d63d49
Simon Kosorinsky <siko@ciklum.com>
2026-06-06 14:53:23 +0200

    Revert bg_pmove.c wall run changes, keep KS timing fixes
    Wall run distance/animation changes were not working correctly.
    Keeping only the KS fixes (g_active.c PMF_JUMP_HELD + cmd.cpp wait).

--- codemp/game/bg_pmove.c
+++ codemp/game/bg_pmove.c
@@ -3052,7 +3052,7 @@ static qboolean PM_CheckJumpLugormod( void )
 			else if ( pm->cmd.forwardmove > 0 //pushing forward
 				&& pm->ps->fd.forceRageRecoveryTime < pm->cmd.serverTime	//not in a force Rage recovery period
 				&& pm->ps->fd.forcePowerLevel[FP_LEVITATION] > FORCE_LEVEL_1 
-				&& PM_WalkableGroundDistance() <= 150 //unfortunately we do not have a happy ground timer like SP (this would use up more bandwidth if we wanted prediction workign right), so we'll just use the actual ground distance.
+				&& PM_WalkableGroundDistance() <= 80 //unfortunately we do not have a happy ground timer like SP (this would use up more bandwidth if we wanted prediction workign right), so we'll just use the actual ground distance.
 				&& (pm->ps->legsAnim == BOTH_JUMP1 || pm->ps->legsAnim == BOTH_INAIR1 ) )//not in a flip or spin or anything
 			{//run up wall, flip backwards
 				if ( allowWallRuns )
@@ -4293,11 +4293,8 @@ static qboolean PM_CheckJump( void )
 			else if ( pm->cmd.forwardmove > 0 //pushing forward -- this is used for wallruns i guess?
 				//&& pm->ps->fd.forceRageRecoveryTime < pm->cmd.serverTime	//not in a force Rage recovery period //JAPRO - Serverside + Clientside - Allow wallrun in rage recovery
 				&& pm->ps->fd.forcePowerLevel[FP_LEVITATION] > FORCE_LEVEL_1
-				&& PM_WalkableGroundDistance() <= 150 //unfortunately we do not have a happy ground timer like SP (this would use up more bandwidth if we wanted prediction workign right), so we'll just use the actual ground distance.
-				&& (pm->ps->legsAnim == BOTH_JUMP1 || pm->ps->legsAnim == BOTH_INAIR1
-					|| pm->ps->legsAnim == BOTH_RUN1 || pm->ps->legsAnim == BOTH_RUN2
-					|| pm->ps->legsAnim == BOTH_RUNBACK1 || pm->ps->legsAnim == BOTH_RUNBACK2
-					|| pm->ps->legsAnim == BOTH_WALK1 || pm->ps->legsAnim == BOTH_WALK2) )//allow wall run from run/walk anims too
+				&& PM_WalkableGroundDistance() <= 80 //unfortunately we do not have a happy ground timer like SP (this would use up more bandwidth if we wanted prediction workign right), so we'll just use the actual ground distance.
+				&& (pm->ps->legsAnim == BOTH_JUMP1 || pm->ps->legsAnim == BOTH_INAIR1 ) )//not in a flip or spin or anything
 			{//run up wall, flip backwards
 				if ( allowWallRuns ) {
 					//FIXME: have to be moving... make sure it's opposite the wall... or at least forward?
@@ -4325,7 +4322,7 @@ static qboolean PM_CheckJump( void )
 					VectorSet(fwdAngles, 0, pm->ps->viewangles[YAW], 0.0f);
 
 					AngleVectors( fwdAngles, fwd, NULL, NULL );
-					VectorMA( pm->ps->origin, 64, fwd, traceto );
+					VectorMA( pm->ps->origin, 32, fwd, traceto );
 
 					pm->trace( &trace, pm->ps->origin, mins, maxs, traceto, pm->ps->clientNum, contents );//FIXME: clip brushes too?
 					VectorSubtract( pm->ps->origin, traceto, idealNormal );
